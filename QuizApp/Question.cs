72700fff07a78ac2c5d21d1d07cb680a69547dca
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-05-26 22:06:26 +0400

    Feat:creating class which will serve as a blueprint for questions

--- QuizApp/Question.cs
+++ QuizApp/Question.cs
@@ -2,6 +2,14 @@ namespace QuizApp
 {
     partial class Question
     {
-        
+        public  string Title {get;}
+        private readonly string [] possibleAnswers = [];
+        private readonly string correctAnswer = "";
+        public Question(string title , string[] possibleAnswers, string correctAnswer)
+        {
+            Title = title;
+            this.possibleAnswers = possibleAnswers;
+            this.correctAnswer = correctAnswer;
+        }
     }
 }
