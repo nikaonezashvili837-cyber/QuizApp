c4a95d762d52f5ad1f6dfc8f1961f877ec51422d
nikaonezashvili837-cyber <nikaonezashvili837@gmail.com>
2026-05-26 22:08:02 +0400

    Creating questions array

--- QuizApp/Program.cs
+++ QuizApp/Program.cs
@@ -4,6 +4,11 @@
     {
         public static void Main()
         {
+            Question[] questionsArray = [
+                new Question("What is capital of France", ["1. New york", "2.Rome", "3.Paris", "4.Berlin"], "3.Paris"),
+                new Question("What is 2 + 2?",[ "1. 3", "2. 4", "3. 5", "4. 6" ],"2. 4"),
+                new Question("Which color is the sky on a clear day?",["1. Green", "2. Blue", "3. Red", "4. Yellow" ],"2. Blue"),
+            ];
             string QuestionBox = @"+----------------------+
 |                                    QUESTION             |
                                    +----------------------+
